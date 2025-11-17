07c1841432dd3d162e65f8f8498086327034dd28
juan.diaz <rulos26@gmail.com>
2025-11-17 15:41:51 -0500

    Update Spanish translations in AdminLTE for login and dashboard messages to enhance user experience and localization accuracy.

--- lang/vendor/adminlte/es/adminlte.php
+++ lang/vendor/adminlte/es/adminlte.php
@@ -14,7 +14,7 @@ return [
     'sign_in' => 'Acceder',
     'log_out' => 'Salir',
     'toggle_navigation' => 'Alternar barra de navegación',
-    'login_message' => 'Autenticarse para iniciar sesión',
+    'login_message' => 'Inicia sesión para comenzar',
     'register_message' => 'Crear una nueva cuenta',
     'password_reset_message' => 'Restablecer la contraseña',
     'reset_password' => 'Restablecer la contraseña',
